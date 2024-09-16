7c39ed885aede7bf5ecaba1775ac93be267044f8
Cilium <85056035+palak-463@users.noreply.github.com>
2024-09-16 13:29:11 +0530

    Create ReadMe.java

---
+++ ReadMe.java
@@ -0,0 +1,81 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReadmeGenerator {
+    public static void main(String[] args) {
+        // Define the content of the README.md file
+        String content = """
+                ### <p align="center"> 👋 Bonjour! </p>
+
+                <div id="Header" align="center">
+                  <img src="https://media.giphy.com/media/paTz7UZbPfTZFRYnnB/giphy.gif" width="200" alt="Coder"/>
+                </div>
+
+                ### <p align="center"> :woman_technologist: About Me: </p>
+
+                <div id="Description" align="center">
+                  :mailbox: Full Stack Developer born and brought up in Pune, Maharashtra, India. <br>
+                  :telescope: B. Tech. student specializing in Computer Engineering. <br>
+                  :seedling: Keen about Problem Solving, Data Structures, and App and Web Development. <br>
+                  :zap: A person who loves to skate and swim during her free time. <br>
+                </div>
+
+                <br>
+
+                <br> 
+
+                ### <p align="center"> :hammer_and_wrench: Languages and Tools: </p>
+
+                <div id="Badges" align="center">
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/c/c-original.svg" title="C" alt="C" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/cplusplus/cplusplus-original.svg" title="CPlusPlus" alt="CPlusPlus" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/java/java-original-wordmark.svg" title="Java" alt="Java" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/python/python-original-wordmark.svg" title="Python" alt="Python" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/html5/html5-original.svg" title="HTML5" alt="HTML5" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/css3/css3-plain-wordmark.svg"  title="CSS3" alt="CSS3" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/javascript/javascript-plain.svg"  title="JavaScript" alt="JavaScript" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/php/php-original.svg" title="PHP" alt="PHP" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/mysql/mysql-original-wordmark.svg" title="MySQL"  alt="MySQL" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/firebase/firebase-plain-wordmark.svg" title="Firebase" alt="Firebase" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/sqlite/sqlite-original-wordmark.svg" title="SQLite" alt="SQLite" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/googlecloud/googlecloud-original-wordmark.svg" title="GoogleCloud" alt="GoogleCloud" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" title="AmazonWebServices" alt="AmazonWebServices" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/android/android-original-wordmark.svg" title="Android" alt="Android" width="50" height="50"/>&nbsp;
+                  <img src="https://github.com/devicons/devicon/blob/master/icons/androidstudio/androidstudio-original-wordmark.svg" title="AndroidStudio" alt="AndroidStudio" width="50" height="50"/>&nbsp;
+                </div>
+
+                <br>
+
+                <br> 
+
+                ### <p align="center"> :fire: My Stats : </p>
+
+                <div id="Stats" align="center">
+
+                  [![GitHub Streak](http://github-readme-streak-stats.herokuapp.com?user=palak-463&theme=highcontrast&border_radius=5&card_width=500&card_height=200)](https://git.io/streak-stats)
+                  
+                  [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=palak-463&layout=compact&theme=highcontrast&border_radius=5&card_width=500&card_height=200)](https://github.com/palak-463/github-readme-stats)
+
+                </div>
+
+                <div id="Connect" align="center">
+                  <h3 align="center">Connect with me:</h3>
+                  <a href="https://linkedin.com/in/palak-pardeshi" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="palak-pardeshi" height="30" width="40" /></a>
+                  <a href="https://www.leetcode.com/palak_pardeshi" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="palak_pardeshi" height="30" width="40" /></a>
+                  <a href="https://auth.geeksforgeeks.org/user/<pardeshipalak463/profile>" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/geeks-for-geeks.svg" alt="<pardeshipalak463/profile>" height="30" width="40" /></a>
+                </div>
+                """;
+
+        // Define the file path
+        String filePath = "README.md";
+
+        // Write the content to the file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(content);
+            System.out.println("README.md file has been generated successfully.");
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing the file: " + e.getMessage());
+        }
+    }
+}
